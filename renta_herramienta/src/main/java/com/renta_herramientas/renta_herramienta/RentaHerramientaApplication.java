4c1527183ffc775a82e13e078763a148b9ffc0f2
LuzSotoBermon <sotolucerito1945@gmail.com>
2026-03-10 16:22:28 -0500

    feat: initialize spring boot project with mysql connection

---
+++ renta_herramienta/src/main/java/com/renta_herramientas/renta_herramienta/RentaHerramientaApplication.java
@@ -0,0 +1,13 @@
+package com.renta_herramientas.renta_herramienta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentaHerramientaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentaHerramientaApplication.class, args);
+	}
+
+}
---
+++ renta_herramienta/src/test/java/com/renta_herramientas/renta_herramienta/RentaHerramientaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renta_herramientas.renta_herramienta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentaHerramientaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
